40ee28157c5ac6bda92a7477f9b4088462d63ce1
John Westerman <john.westerman@gmail.com>
2018-06-28 12:54:36 -0500

    6-29-18 2

--- caseStudy1.1.2.bs.py
+++ caseStudy1.1.2.bs.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup
 import urllib.request
-from html.parser import HTMLParser
 
 
 htmlDoc = urllib.request.urlopen('https://www.eecs.mit.edu/people/faculty-advisors').read().decode('utf-8')
@@ -14,12 +13,21 @@ for a in sp:
 
 print(profs)
 
-#url = 'http://export.arxiv.org/api/query?search_query=all:electron&start=0&max_results=1'
-#url = 'http://export.arxiv.org/api/query?search_query=au:Arvind+AND+cat:eess'
-#url = 'http://arxiv.org/find/(subject)/1/au:+(lastname)_(initial)/0/1/0/all/0/1'
-url = 'http://arxiv.org/find/eess/1/au:+Arvind/0/1/0/all/0/1'
-data = urllib.request.urlopen(url).read()
-print(data)
+authUrl = 'http://arxiv.org/find/eess/1/au:+Arvind/0/1/0/all/0/1'
+authData = urllib.request.urlopen(authUrl).read().decode('utf-8')
+print(authData)
+
+absUrl = 'https://arxiv.org/abs/1712.08227'
+absDoc = urllib.request.urlopen(absUrl).read().decode('utf-8')
+soup = BeautifulSoup(absDoc, 'html.parser')
+
+absWords = []
+
+sp = soup.find_all("blockquote", class_="abstract mathjax")
+for a in sp:
+    absWords.append(a.contents[2])
+
+print("absWords", absWords)
 
 
 
