971c0cc83fcd5430393a7e4b133d7d1284a0d8aa
John Westerman <john.westerman@gmail.com>
2018-06-30 07:57:41 -0500

    6-30-18

--- caseStudy1.1.2.bs.py
+++ caseStudy1.1.2.bs.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import urllib.request
 
+# changes to commit
 
 htmlDoc = urllib.request.urlopen('https://www.eecs.mit.edu/people/faculty-advisors').read().decode('utf-8')
 soup = BeautifulSoup(htmlDoc, 'html.parser')
